105b2e7d409dfa04224fb34f61531ff20fa5face
Lim Zhi Pei <95734402+limzhipei@users.noreply.github.com>
2022-10-07 16:47:43 +0800

    Update weld_cyl_batt_inspect_analysis.py

--- weld_cyl_batt_inspect_analysis.py
+++ weld_cyl_batt_inspect_analysis.py
@@ -50,6 +50,7 @@ samples_array = np.arange(1, samples+1)
 
 if (samples == 0 or selector is None or uploaded_file is None):
      st.header('Please input number of samples, select camera and upload a file to start!')
+     st.write('Battery number must be consecutive in the test run.')
 
 else:
      if cam1_selected == True:
